5e0a3f8e72259602f7ad1c4162d74016e2c6ac41
dickinsona19 <dickinsona19@yahoo.com>
2022-09-27 11:22:05 -0400

    Player has ability to jump, but only when on ground

--- MainWorld/Main.gd
+++ MainWorld/Main.gd
@@ -14,3 +14,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_Area2D_body_entered(body):
+	if body.name == "Player":
+		$Player.canJump = true
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,9 @@
 extends KinematicBody2D
 
-const GRAVITY = 200.0
+const GRAVITY = 300.0
 var velocity = Vector2()
+const jump =500
+var canJump = false
 
 
 # Called when the node enters the scene tree for the first time.
@@ -12,13 +14,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("move_right"):
-		position.x +=1
+		position.x +=5
 	if Input.is_action_pressed("move_left"):
-		position.x -=1
+		position.x -=5
+		
 	velocity.y += delta * GRAVITY
+	
 	var motion = velocity * delta
+	
+	if Input.is_action_just_pressed("jump") and canJump:
+		velocity.y -= jump
+		canJump = false
+		
 	move_and_collide(motion)
-
+	if velocity.y >= 200:
+		velocity.y = 200
 
 func _on_KinematicBody2D_mouse_entered():
 	print("Body")
